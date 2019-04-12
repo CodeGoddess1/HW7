1c3ee35359c1cbcf2270ad84aa8eb33137cac88b
CodeGoddess1 <48345161+CodeGoddess1@users.noreply.github.com>
2019-04-11 20:08:32 -0400

    Add files via upload

---
+++ HW7/Palindrome.Console/Palindrome.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Palindrome
+    {
+        public string GetText(string msg)
+        {
+            try
+            {
+                string textInfo = string.Empty;
+                char[] chArray = msg.ToCharArray();
+                char[] PalChar = new char[chArray.Length];
+                PalChar = chArray;
+                textInfo = msg;
+
+                Array.Reverse(PalChar, 0, PalChar.Length);
+                return new string(PalChar);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An Error Occurred");
+                return null;
+            }
+           
+            
+        }
+        public bool IsPalindrome(string userInput, string userInputReversed)
+        {
+            try
+            {
+                if (userInput == userInputReversed)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("An Error Occurred");
+                return false;
+            }
+            
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ HW7/Palindrome.Console/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string EnterWord = string.Empty;
+            string Exit = string.Empty;
+            string EnterWordReversed = string.Empty;
+              do
+            {
+                try
+                {
+                    Console.Write("Enter text to see if it’s a palindrome    ");
+                    EnterWord = Console.ReadLine();
+                    Palindrome Paldrm = new Palindrome();
+                    EnterWordReversed = Paldrm.GetText(EnterWord);
+
+                    if (Paldrm.IsPalindrome(EnterWord, EnterWordReversed) == true)
+                    {
+                        Console.WriteLine("Its A Palindrome");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Its Not A Palindrome");
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("An Error Occurred");
+                }
+                
+              
+                 Console.WriteLine(" If you want to Exit type N");
+                 Exit = Console.ReadLine();
+
+            } while (Exit!="N");
+        }
+    }
+}
